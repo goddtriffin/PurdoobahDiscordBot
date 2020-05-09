9e53642085ad04382d5ba552ef584d1e7aa7337e
Todd Everett Griffin <goddtriffin@gmail.com>
2020-05-09 05:08:54 -0500

    Add support for help command. Update the display of the YMSH command to be hidden by a spoiler tag.

--- cmd/bot/purdoobah-bot.go
+++ cmd/bot/purdoobah-bot.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"math/rand"
 	"strings"
@@ -42,19 +43,60 @@ func NewPurdoobahBot(botToken string) (*PurdoobahBot, error) {
 }
 
 func (pb *PurdoobahBot) mux(s disgord.Session, evt *disgord.MessageCreate) {
+	if len(evt.Message.Content) == 0 {
+		return
+	}
+
 	command := strings.ToLower(strings.Fields(evt.Message.Content)[0])
+
+	if command == "!help" {
+		pb.replyHelp(s, evt)
+	}
 
 	if command == "!ymsh" {
 		pb.replyYMSH(s, evt)
 	}
 }
 
+func (pb *PurdoobahBot) reply(s disgord.Session, evt *disgord.MessageCreate, reply interface{}) {
+	_, err := evt.Message.Reply(context.Background(), s, reply)
+	if err != nil {
+		log.Printf("reply error: %+v\n", err)
+	}
+}
+
+func (pb *PurdoobahBot) replyHelp(s disgord.Session, evt *disgord.MessageCreate) {
+	fields := []*disgord.EmbedField{}
+	for _, command := range pb.getCommands() {
+		fields = append(fields, &disgord.EmbedField{
+			Name:   command.String(),
+			Value:  fmt.Sprintf("`%s`", command.description),
+			Inline: true,
+		})
+	}
+
+	help := &disgord.Embed{
+		Description: "**PurdoobahBot Commands**",
+		Color:       15844367,
+		Fields:      fields,
+		Thumbnail: &disgord.EmbedThumbnail{
+			URL: "https://www.purdoobahs.com/res/image/logo/purdoobahs-white-768x768.png",
+		},
+	}
+
+	log.Printf("%s (%s) called !help\n", evt.Message.Author.Username, evt.Message.Author.ID)
+	pb.reply(s, evt, help)
+}
+
 func (pb *PurdoobahBot) replyYMSH(s disgord.Session, evt *disgord.MessageCreate) {
 	ymsh := pb.ymsh.String(pb.rand)
-	log.Println("YMSH:", ymsh)
+	log.Printf("%s (%s) called !YMSH: %s\n", evt.Message.Author.Username, evt.Message.Author.ID, ymsh)
+	pb.reply(s, evt, fmt.Sprintf("YMSH stands for...\n||%s||", ymsh))
+}
 
-	_, err := evt.Message.Reply(context.Background(), s, ymsh)
-	if err != nil {
-		log.Printf("YMSH Reply error: %+v\n", err)
+func (pb *PurdoobahBot) getCommands() []command {
+	return []command{
+		{"help", "lists PurdoobahBot commands"},
+		{"YMSH", "secret YMSH definition"},
 	}
 }
