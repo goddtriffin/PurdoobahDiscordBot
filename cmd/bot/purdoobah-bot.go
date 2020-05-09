69e7a71cbf69e7e47927b7bebcf7e131cb0e157d
Todd Everett Griffin <goddtriffin@gmail.com>
2020-05-09 05:38:20 -0500

    Add support for pr, instagram, facebook, and youtube commands.

--- cmd/bot/purdoobah-bot.go
+++ cmd/bot/purdoobah-bot.go
@@ -14,10 +14,12 @@ import (
 // PurdoobahBot is the Discord PurdoobahBot.
 type PurdoobahBot struct {
 	*disgord.Client
-
 	rand *rand.Rand
 
-	ymsh *ymsh
+	commands     []command
+	thumbnailURL string
+	socialMedia  map[string]string // name -> URL
+	ymsh         *ymsh
 }
 
 // NewPurdoobahBot creates a new PurdoobahBot.
@@ -34,6 +36,24 @@ func NewPurdoobahBot(botToken string) (*PurdoobahBot, error) {
 			Logger:      disgord.DefaultLogger(false),
 		}),
 		rand: rand.New(rand.NewSource(time.Now().UnixNano())),
+
+		commands: []command{
+			{"help", "displays help"},
+			{"commands", "displays commands"},
+			{"YMSH", "secret definition of YMSH"},
+			{"pr", "links social media"},
+			{"website", "links official website"},
+			{"instagram", "links official website"},
+			{"facebook", "links official website"},
+			{"youtube", "links official website"},
+		},
+		thumbnailURL: "https://www.purdoobahs.com/res/image/logo/purdoobahs-white-768x768.png",
+		socialMedia: map[string]string{
+			"website":   "https://www.purdoobahs.com/",
+			"instagram": "https://www.instagram.com/purdoobahs/",
+			"facebook":  "https://www.facebook.com/purdoobahs/",
+			"youtube":   "https://www.youtube.com/channel/UCIH2OACGjUeDPfkISb_lp_Q",
+		},
 		ymsh: ymsh,
 	}
 
@@ -56,8 +76,16 @@ func (pb *PurdoobahBot) mux(s disgord.Session, evt *disgord.MessageCreate) {
 		pb.replyCommands(s, evt)
 	case "!ymsh":
 		pb.replyYMSH(s, evt)
+	case "!pr":
+		pb.replyPR(s, evt)
 	case "!website":
 		pb.replyWebsite(s, evt)
+	case "!instagram":
+		pb.replyInstagram(s, evt)
+	case "!facebook":
+		pb.replyFacebook(s, evt)
+	case "!youtube":
+		pb.replyYoutube(s, evt)
 	}
 }
 
@@ -69,31 +97,29 @@ func (pb *PurdoobahBot) reply(s disgord.Session, evt *disgord.MessageCreate, rep
 }
 
 func (pb *PurdoobahBot) replyHelp(s disgord.Session, evt *disgord.MessageCreate) {
-	help := &disgord.Embed{
+	embedHelp := &disgord.Embed{
 		Description: "**PurdoobahBot Help**",
 		Color:       15844367,
 		Fields: []*disgord.EmbedField{
-			{Name: "!commands", Value: "displays commands"},
+			{Name: "`!commands`", Value: "displays commands"},
 		},
-		Thumbnail: &disgord.EmbedThumbnail{
-			URL: "https://www.purdoobahs.com/res/image/logo/purdoobahs-white-768x768.png",
-		},
+		Thumbnail: &disgord.EmbedThumbnail{URL: pb.thumbnailURL},
 	}
 
 	log.Printf("%s (%s) called !help\n", evt.Message.Author.Username, evt.Message.Author.ID)
-	pb.reply(s, evt, help)
+	pb.reply(s, evt, embedHelp)
 }
 
 func (pb *PurdoobahBot) replyCommands(s disgord.Session, evt *disgord.MessageCreate) {
 	fields := []*disgord.EmbedField{}
-	for _, command := range pb.getCommands() {
+	for _, command := range pb.commands {
 		fields = append(fields, &disgord.EmbedField{
 			Name:  fmt.Sprintf("`%s`", command.String()),
-			Value: fmt.Sprintf("%s", command.description),
+			Value: command.description,
 		})
 	}
 
-	help := &disgord.Embed{
+	embedCommands := &disgord.Embed{
 		Description: "**PurdoobahBot Commands**",
 		Color:       15844367,
 		Fields:      fields,
@@ -103,25 +129,49 @@ func (pb *PurdoobahBot) replyCommands(s disgord.Session, evt *disgord.MessageCre
 	}
 
 	log.Printf("%s (%s) called !commands\n", evt.Message.Author.Username, evt.Message.Author.ID)
-	pb.reply(s, evt, help)
+	pb.reply(s, evt, embedCommands)
 }
 
 func (pb *PurdoobahBot) replyYMSH(s disgord.Session, evt *disgord.MessageCreate) {
 	ymsh := pb.ymsh.String(pb.rand)
 	log.Printf("%s (%s) called !YMSH: %s\n", evt.Message.Author.Username, evt.Message.Author.ID, ymsh)
-	pb.reply(s, evt, fmt.Sprintf("YMSH stands for...\n||%s||", ymsh))
+	pb.reply(s, evt, fmt.Sprintf("YMSH stands for... ||%s||", ymsh))
 }
 
+func (pb *PurdoobahBot) replyPR(s disgord.Session, evt *disgord.MessageCreate) {
+	fields := []*disgord.EmbedField{}
+	for name, URL := range pb.socialMedia {
+		fields = append(fields, &disgord.EmbedField{
+			Name:  strings.Title(strings.ToLower(name)),
+			Value: URL,
+		})
+	}
+
+	embedPR := &disgord.Embed{
+		Description: "**PurdoobahBot Commands**",
+		Color:       15844367,
+		Fields:      fields,
+		Thumbnail: &disgord.EmbedThumbnail{
+			URL: "https://www.purdoobahs.com/res/image/logo/purdoobahs-white-768x768.png",
+		},
+	}
+
+	log.Printf("%s (%s) called !pr\n", evt.Message.Author.Username, evt.Message.Author.ID)
+	pb.reply(s, evt, embedPR)
+}
 func (pb *PurdoobahBot) replyWebsite(s disgord.Session, evt *disgord.MessageCreate) {
 	log.Printf("%s (%s) called !website\n", evt.Message.Author.Username, evt.Message.Author.ID)
-	pb.reply(s, evt, fmt.Sprintf("%s", "https://www.purdoobahs.com/"))
+	pb.reply(s, evt, pb.socialMedia["website"])
 }
-
-func (pb *PurdoobahBot) getCommands() []command {
-	return []command{
-		{"help", "displays help"},
-		{"commands", "displays commands"},
-		{"YMSH", "secret YMSH definition"},
-		{"website", "links official website"},
-	}
+func (pb *PurdoobahBot) replyInstagram(s disgord.Session, evt *disgord.MessageCreate) {
+	log.Printf("%s (%s) called !instagram\n", evt.Message.Author.Username, evt.Message.Author.ID)
+	pb.reply(s, evt, pb.socialMedia["instagram"])
+}
+func (pb *PurdoobahBot) replyFacebook(s disgord.Session, evt *disgord.MessageCreate) {
+	log.Printf("%s (%s) called !facebook\n", evt.Message.Author.Username, evt.Message.Author.ID)
+	pb.reply(s, evt, pb.socialMedia["facebook"])
+}
+func (pb *PurdoobahBot) replyYoutube(s disgord.Session, evt *disgord.MessageCreate) {
+	log.Printf("%s (%s) called !youtube\n", evt.Message.Author.Username, evt.Message.Author.ID)
+	pb.reply(s, evt, pb.socialMedia["youtube"])
 }
